81fbc2feab0b63f8bfb2d7928a8194cfe3ea01d6
Ferris Boran <ferris.boran@gmail.com>
2019-03-12 19:28:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,10 +79,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] => {
-	   :known_for => "Ruby",
-	   :languages => ["LISP", "C"]
-	 }
+	programmer_hash[:yukihiro_matsumoto] = {}
+  
 end
 
 def changing_alan
